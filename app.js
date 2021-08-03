4d9f0f246c1d127af61dbfaa1edeb2fb6759d9c2
Nattawanee <nattawanee.sks@mail.kmutt.ac.th>
2021-08-03 20:24:58 +0700

    [ADD] Note and route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var analyticRouter = require("./routes/feeling_analytic");
+var noteRouter = require("./routes/note");
 
 var app = express();
 
@@ -22,7 +23,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/profile", usersRouter);
-app.use("/analytic", analyticRouter);
+app.use("/profile/analytic", analyticRouter);
+app.use("/profile/note", noteRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
