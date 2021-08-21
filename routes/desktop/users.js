a96b7e66257ca27e7a93cb19cc165478578ff5e5
Poi <o.onepiece9@gmail.com>
2021-08-21 18:52:53 +0700

    [ADD] Show user profile

--- routes/desktop/users.js
+++ routes/desktop/users.js
@@ -21,12 +21,75 @@ router.get("/", async (req, res, next) => {
       });
     });
 
+    const profileListRef = db.collection("User");
+    const profileList = [];
+    const getUserID = "6";
+    let testReq = req.params.userID;
+    console.log(testReq);
+    await profileListRef.get().then((snapshot) => {
+      snapshot.forEach((doc) => {
+        if (doc.data().userID == getUserID) {
+          profileList.push({
+            firstName: doc.data().firstName,
+            lastName: doc.data().lastName,
+            TelNo: doc.data().TelNo,
+            Email: doc.data().Email,
+            contactNote: doc.data().contactNote,
+          });
+        }
+      });
+    });
+    console.log("------");
+    console.log(profileList);
+
     res.render("desktop/user_profile", {
       contactlists,
+      profileList,
     });
   } catch (error) {
     console.log(error);
   }
 });
+
+// router.post("/:id", async (req, res, next) => {
+//   try {
+//     const contactListRef = db.collection("User");
+//     const contactlists = [];
+
+//     await contactListRef.get().then((snapshot) => {
+//       snapshot.forEach((doc) => {
+//         contactlists.push({
+//           userID: doc.data().userID,
+//           nickName: doc.data().nickName,
+//         });
+//       });
+//     });
+
+//     const profileListRef = db.collection("User");
+//     const profileList = [];
+//     const getUserID = "2";
+//     let testReq = req.params.userID;
+//     console.log(testReq);
+//     await profileListRef.get().then((snapshot) => {
+//       snapshot.forEach((doc) => {
+//         if (doc.data().userID == getUserID) {
+//           profileList.push({
+//             firstName: doc.data().firstName,
+//             lastName: doc.data().lastName,
+//             TelNo: doc.data().TelNo,
+//             Email: doc.data().Email,
+//           });
+//         }
+//       });
+//     });
+//     console.log("------");
+//     console.log(profileList);
+//     res.render("desktop/user_profile", {
+//       contactlists,
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 module.exports = router;
