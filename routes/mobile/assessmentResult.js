42cc33a314118485f8534fbdfab782c3bc64b61c
Nattawanee <nattawanee.sks@mail.kmutt.ac.th>
2021-08-28 15:00:51 +0700

    [EDIT] add score to Database

--- routes/mobile/assessmentResult.js
+++ routes/mobile/assessmentResult.js
@@ -3,6 +3,13 @@ var router = express.Router();
 const { db } = require("../../Database/database");
 
 router.get("/", (req, res, next) => {
+  console.log(req.query.score);
+  var assessmentScore = req.query.score;
+  const data = {
+    score: assessmentScore
+  };
+  //db.collection('Assessment').add(data);
+  db.collection('User').doc('user2').collection('assessment').add(data);
   res.render("mobile/assessmentResult");
 });
 
