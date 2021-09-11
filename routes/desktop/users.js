49807b9d13baec20d9cfb4b9afee32482ea85521
Poi <o.onepiece9@gmail.com>
2021-09-11 14:04:54 +0700

    [FIX] Fix bug Path with userID

--- routes/desktop/users.js
+++ routes/desktop/users.js
@@ -39,8 +39,6 @@ router.get("/", async (req, res, next) => {
         }
       });
     });
-    console.log("------");
-    console.log(profileList);
 
     res.render("desktop/user_profile", {
       contactlists,
@@ -83,11 +81,126 @@ router.get("/:userID", async (req, res, next) => {
         }
       });
     });
-    console.log("------");
-    console.log(profileList);
+
     res.render("desktop/user_profile", {
       contactlists,
       profileList,
+      getUserID,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/:userID/analytic", async (req, res, next) => {
+  try {
+    const contactListRef = db.collection("User");
+    const contactlists = [];
+    const getUserID = req.params.userID;
+    await contactListRef.get().then((snapshot) => {
+      snapshot.forEach((doc) => {
+        contactlists.push({
+          userID: doc.data().userID,
+          nickName: doc.data().nickName,
+        });
+      });
+    });
+
+    res.render("desktop/feeling_analytic", {
+      contactlists,
+      getUserID,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/:userID/appointment", async (req, res, next) => {
+  try {
+    const contactListRef = db.collection("User");
+    const contactlists = [];
+    const getUserID = req.params.userID;
+    await contactListRef.get().then((snapshot) => {
+      snapshot.forEach((doc) => {
+        contactlists.push({
+          userID: doc.data().userID,
+          nickName: doc.data().nickName,
+        });
+      });
+    });
+
+    res.render("desktop/appointment_all", {
+      contactlists,
+      getUserID,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/:userID/note", async (req, res, next) => {
+  try {
+    const contactListRef = db.collection("User");
+    const contactlists = [];
+    const getUserID = req.params.userID;
+    await contactListRef.get().then((snapshot) => {
+      snapshot.forEach((doc) => {
+        contactlists.push({
+          userID: doc.data().userID,
+          nickName: doc.data().nickName,
+        });
+      });
+    });
+
+    res.render("desktop/all_note", {
+      contactlists,
+      getUserID,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/:userID/note/content", async (req, res, next) => {
+  try {
+    const contactListRef = db.collection("User");
+    const contactlists = [];
+    const getUserID = req.params.userID;
+    await contactListRef.get().then((snapshot) => {
+      snapshot.forEach((doc) => {
+        contactlists.push({
+          userID: doc.data().userID,
+          nickName: doc.data().nickName,
+        });
+      });
+    });
+
+    res.render("desktop/note_content", {
+      contactlists,
+      getUserID,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/:userID/note/content/edit", async (req, res, next) => {
+  try {
+    const contactListRef = db.collection("User");
+    const contactlists = [];
+    const getUserID = req.params.userID;
+    await contactListRef.get().then((snapshot) => {
+      snapshot.forEach((doc) => {
+        contactlists.push({
+          userID: doc.data().userID,
+          nickName: doc.data().nickName,
+        });
+      });
+    });
+
+    res.render("desktop/note_edit", {
+      contactlists,
+      getUserID,
     });
   } catch (error) {
     console.log(error);
