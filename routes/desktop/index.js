68bc4b8b2ef49ae8a2d60f8322c4f7aa1c59714b
Poi <o.onepiece9@gmail.com>
2021-11-19 22:56:36 +0700

    [ADD] Show Risk User

--- routes/desktop/index.js
+++ routes/desktop/index.js
@@ -38,25 +38,62 @@ router.get("/", async (req, res, next) => {
 
     console.log(userLists);
     let messageUserListRef;
+    let assessmentUserListsRef;
     const messageUserLists = [];
     const assessmentUserLists = [];
     for (let x = 0; x < userLists.length; x++) {
-      messageUserListRef = db
-        .collection("User")
-        .doc(userLists[x].userID)
-        .collection("message");
-      await messageUserListRef.get().then((snapshot) => {
-        snapshot.forEach((doc) => {
-          messageUserLists.push({
-            lineName: userLists[x].lineName,
-            emotion: doc.data().emotion,
-            timestamp: doc.data().timestamp,
+      if (userLists[x].lineName != "") {
+        messageUserListRef = db
+          .collection("User")
+          .doc(userLists[x].userID)
+          .collection("message");
+        await messageUserListRef.get().then((snapshot) => {
+          snapshot.forEach((doc) => {
+            messageUserLists.push({
+              lineName: userLists[x].lineName,
+              emotion: doc.data().emotion,
+              timestamp: doc.data().timestamp,
+            });
           });
         });
-      });
-    }
-    console.log(messageUserLists);
+        assessmentUserListsRef = db
+          .collection("User")
+          .doc(userLists[x].userID)
+          .collection("assessment")
+          .where("status", "==", "Danger");
 
+        await assessmentUserListsRef.get().then((snapshot) => {
+          snapshot.forEach((doc) => {
+            if (doc.data().type == "dass") {
+              assessmentUserLists.push({
+                lineName: userLists[x].lineName,
+                type: doc.data().type,
+                timestamp: doc.data().timestamp,
+                status: doc.data().status,
+                Ascore: doc.data().Ascore,
+                Dscore: doc.data().Dscore,
+                Sscore: doc.data().Sscore,
+                scoreTotal:
+                  Number(doc.data().Ascore) +
+                  Number(doc.data().Dscore) +
+                  Number(doc.data().Sscore),
+              });
+            } else if (doc.data().type == "depress") {
+              assessmentUserLists.push({
+                lineName: userLists[x].lineName,
+                type: doc.data().type,
+                timestamp: doc.data().timestamp,
+                status: doc.data().status,
+                score: doc.data().score,
+              });
+            }
+          });
+        });
+      }
+    }
+    console.log(assessmentUserLists);
+
+    // ดึงข้อมูล Feeling down ไปแสดงในหน้า  Index
     const feelingDownLists = [];
     let scoreEmotion = 0;
     let countIndexList = 0;
@@ -80,11 +117,57 @@ router.get("/", async (req, res, next) => {
       }
     }
 
-    console.log(feelingDownLists);
     feelingDownLists.sort(function (a, b) {
       return a.emotion - b.emotion;
     });
-    console.log("-" + feelingDownLists.length);
+
+    // ดึงข้อมูล Risk User ไปแสดงในหน้า  Index
+    const riskUserLists = [];
+    let score = 0;
+    let countIndexLists = 0;
+    for (let K = 0; K < assessmentUserLists.length; K++) {
+      if (K == 0) {
+        if (assessmentUserLists[K].type == "dass") {
+          riskUserLists[0] = assessmentUserLists[0];
+          score = Number(assessmentUserLists[0].scoreTotal);
+          riskUserLists[0].scoreTotal = score;
+        } else if (assessmentUserLists[K].type == "depress") {
+          riskUserLists[0] = assessmentUserLists[0];
+          score = Number(assessmentUserLists[0].score);
+          riskUserLists[0].score = score;
+        }
+      } else if (K != 0) {
+        if (
+          assessmentUserLists[K - 1].lineName != assessmentUserLists[K].lineName
+        ) {
+          countIndexLists++;
+          if (assessmentUserLists[K].type == "dass") {
+            riskUserLists[countIndexLists] = assessmentUserLists[K];
+            score = Number(assessmentUserLists[K].scoreTotal);
+            riskUserLists[countIndexLists].scoreTotal = score;
+          } else if (assessmentUserLists[K].type == "depress") {
+            riskUserLists[countIndexLists] = assessmentUserLists[K];
+            score = Number(assessmentUserLists[K].score);
+            riskUserLists[countIndexLists].score = score;
+          }
+        } else if (
+          assessmentUserLists[K - 1].lineName == assessmentUserLists[K].lineName
+        ) {
+          if (assessmentUserLists[K].type == "dass") {
+            score = score + Number(assessmentUserLists[K].scoreTotal);
+            riskUserLists[countIndexLists].scoreTotal = score;
+          } else if (assessmentUserLists[K].type == "depress") {
+            score = score + Number(assessmentUserLists[K].score);
+            riskUserLists[countIndexLists].score = score;
+          }
+        }
+      }
+    }
+
+    riskUserLists.sort(function (a, b) {
+      return b.emotion - a.emotion;
+    });
+
     const contactListRef = db.collection("User");
     const contactlists_temp = [];
     const contactlists = [];
@@ -126,6 +209,7 @@ router.get("/", async (req, res, next) => {
       res.render("desktop/index", {
         contactlists,
         feelingDownLists,
+        riskUserLists,
       });
     } else {
       await contactListRef.get().then((snapshot) => {
@@ -142,6 +226,7 @@ router.get("/", async (req, res, next) => {
       res.render("desktop/index", {
         contactlists,
         feelingDownLists,
+        riskUserLists,
       });
     }
   } catch (error) {
