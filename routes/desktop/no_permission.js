092fa09c9ad76acca0fb053e8954923aa6a63487
Poi <o.onepiece9@gmail.com>
2021-11-23 19:55:34 +0700

    [DELETE] Delete All  Unused File

--- routes/desktop/no_permission.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const { db } = require("../../Database/database");
-
-router.get("/", (req, res, next) => {
-  res.render("desktop/nopermission");
-});
-
-module.exports = router;
